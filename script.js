2c7ff0fde352d23326d2bd9be2f7985bfbf04068
azzaza <yasko.zakhar@gmail.com>
2021-06-16 14:37:18 +0300

    add_ .

---
+++ script.js
@@ -0,0 +1,24 @@
+// const measege=document.querySelector('.measege')
+// const chat=document.querySelector('.main_chat')
+// measege.addEventListener('keyup',e=>{
+//     if(e.key=='Enter'){
+//          chat.innerHTML+=
+//     `
+//     <div class="message_gl" >
+//         <img src="img/footer/1.jpg" class="chatp">
+//         <div class="message">
+//             <div class="user">
+//                 <h4>Yasko Zakhar </h4>
+//                     <h6>14:15 PM</h6>
+//             </div>
+//             <div class="message_cont">
+//                 <p class="n_txt">
+//                     ${measege.value}
+//                 </p>
+//             </div>
+//         </div>
+//     </div>
+//     `
+//     }
+   
+// })
